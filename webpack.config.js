4e748ea4004bd27b6f8d074698512b01a12bec24
yiershan <511176294@qq.com>
2018-05-18 11:34:39 +0800

    使用webpack结合ts-loader打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
 var webpack = require('webpack')
 module.exports = {
     entry: {
-        index: './index.js',
-        test: './test.js'
+        index: './index.ts',
+        test: './test.ts'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
     },
     output: {
         filename: '[name].js'
